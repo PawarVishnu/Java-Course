a88ea84ad86c89e5c5b1a81872d61ec49ae68551
PawarVishnu <117418644+PawarVishnu@users.noreply.github.com>
2025-08-03 18:21:12 +0530

    Update test.java

--- src/Demo/test.java
+++ src/Demo/test.java
@@ -7,12 +7,18 @@ public class test {
 	public static void main (String [] args ) {
 		
 		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter Student Full Name :");
+		
+		String name = sc.nextLine();
 		
 		System.out.print("enter your gender male/female :");
 		
 		char gender = sc.next().charAt(0);
+
+		System.out.println("full name of the Student :" + name);
 		
-		System.out.println("full name of the Student :" + gender);
+		System.out.println("Student Gender :" + gender);
 		
 		sc.close();
 	}
